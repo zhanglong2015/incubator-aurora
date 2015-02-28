d36893d23a6a8022d805e35d10c3e00c91939754
jyang12 <jyang12@ebay.com>
2015-02-28 16:38:21 +0800

    add role function

--- src/main/java/org/apache/aurora/scheduler/async/TaskContext.java
+++ src/main/java/org/apache/aurora/scheduler/async/TaskContext.java
@@ -1,7 +1,10 @@
 package org.apache.aurora.scheduler.async;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+
+import com.google.common.collect.Ordering;
 
 import org.apache.aurora.scheduler.mesos.TrackableResource;
 import org.apache.mesos.Protos.Offer;
@@ -9,15 +12,32 @@ import org.apache.mesos.Protos.Resource;
 
 public class TaskContext {
 	private List<TrackableResource> trackableResource = new LinkedList<TrackableResource>();
+	private static final String DEFAULT_MESOS_ROLE = "*";
 	
 	public TaskContext(Offer offer) {
-    List<Resource> resources = offer.getResourcesList();
+    List<Resource> resources = offer.getResourcesList();    
     for (Resource resource : resources) {
     	trackableResource.add(new TrackableResource(resource));
     }
+    trackableResource = ROLE_FIRST.sortedCopy(trackableResource);
 	}
 	
   public List<TrackableResource> getTrackableResources() {
   	return trackableResource;
   }
+  
+  private static final Ordering<TrackableResource> ROLE_FIRST = Ordering.from(
+      new Comparator<TrackableResource>() {
+        @Override
+        public int compare(TrackableResource r0, TrackableResource r1) {
+        	 if (r0.getResource().getRole().equals(r1.getResource().getRole())) {
+             return r0.getResource().getName().compareTo(r1.getResource().getName());
+         }
+         if (DEFAULT_MESOS_ROLE.equals(r0.getResource().getRole())) {
+             return 1;
+         } else {
+             return -1;
+         }
+        }
+      });  
 }
